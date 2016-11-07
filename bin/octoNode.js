670008b062f29453ab9559a459e596a02283ac9b
alu0100786330 <alu0100786330@ull.edu.es>
2016-11-07 13:16:55 +0000

    comiteando para traer pull

--- bin/octoNode.js
+++ bin/octoNode.js
@@ -4,16 +4,17 @@ module.exports = {
  
  
  octo: () => {
-     
-     
-     github.auth.config({ username, password }).login({
-          scopes: ['user', 'repo'],
-          note: 'Token para Gitbook'
-        }, (err, id, token) => {
-          if (err) return err;
-          console.log(err);
-          console.log(id);
-          console.log(token); // Ahora si tenemos el token de github!!
-        });
- }
+		var github = require('octonode');    
+		 
+		github.auth.config({ username, password }).login({
+		  scopes: ['user', 'repo'],
+		  note: 'Token para Gitbook'
+		}, 
+		(err, id, token) => {
+		  if (err) return err;
+		  console.log(err);
+		  console.log(id);
+		  console.log(token); // Ahora si tenemos el token de github!!
+		});
+	}
 };
